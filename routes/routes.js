518fec4f88ec637419ea45ac21dd56b39b302c98
malavikarajeshvikraman <malu.rajesh2000@gmail.com>
2020-12-31 10:18:14 +0530

    Total change

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,10 @@ routes.get("/", function(req, res){
 routes.get("/login", function(req, res){
     res.render("login.ejs"); //Every EJS file must be in the views folder. EJS is our view engine
 });
+
+routes.get("/roomspage", function(req, res){
+    res.render("roomspage.ejs"); 
+});
 routes.get("/*", function(req, res){
     res.send("You went to a route that doesn't exist. Shame on you.");
 });
