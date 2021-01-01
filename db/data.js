b78a0ab515ca62f88dddd18596510728af9b2631
rray14 <44109161+rray14@users.noreply.github.com>
2021-01-01 23:42:18 +0530

    main ayi poi

--- db/data.js
+++ db/data.js
@@ -1,16 +1,16 @@
-const mysql =require('mysql');
-var conn = mysql.createConnection({
-    connectionList:10,
-    password:'12345',
-    user:'root',
-    database:'hotel',
-    host:'localhost',
-    port:'3306'
+// const mysql =require('mysql');
+// var conn = mysql.createConnection({
+//     connectionList:10,
+//     password:'12345',
+//     user:'root',
+//     database:'hotel',
+//     host:'localhost',
+//     port:'3306'
 
-})
+// })
 
-conn.connect(function(err) {
-    if (err) throw err;
-    console.log('Database is connected successfully !');
-  });
-  module.exports = conn;
+// conn.connect(function(err) {
+//     if (err) throw err;
+//     console.log('Database is connected successfully !');
+//   });
+//   module.exports = conn;
