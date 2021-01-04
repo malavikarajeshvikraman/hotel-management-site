615865722e7561ee2b97b35db4bff3ece8783a96
malavikarajeshvikraman <malu.rajesh2000@gmail.com>
2021-01-04 19:04:50 +0530

    Finally

--- routes/booking.js
+++ routes/booking.js
@@ -5,46 +5,39 @@ const routes = express.Router()
 var async=require("async");
 
 routes.get("/booking", function(req, res){
-    var max =[6]
+    var max =[]
     res.render("booking.ejs",{maximum:max}); 
 });
-function getmaximum( cb) { 
-    var pending = 6;
-    var max = [6];
-    var i;
-    for (i=1;i<=6;i++){
-    var sql='select count(roomno) as counter from room where roomtypeid = (?) and roomno not in ( select roomno from reservation where ( date_checkout > (?) ) and ( date_checkin < (?)));';
-     db.query(sql, [i,startdate , enddate], function (err, data, fields) {
-        if(err) throw err
-        data .forEach(function(v){ max[i] = parseInt(v.counter); })
-        console.log(max[i]);
-    });
-    if( 0 === --pending ) {
-        cb(max); //
-    }
-}
-}
+
 
 routes.get("/bookingstatus", function(req, res){
     var startdate = req.body.trip_start;
     var enddate =  req.body.trip_end;
     var i;
-    var max = [6];
-   /* for (i=1;i<=6;i++){
+    var max= []
+   for (i=1;i<=6;i++){
     var sql='select count(roomno) as counter from room where roomtypeid = (?) and roomno not in ( select roomno from reservation where ( date_checkout > (?) ) and ( date_checkin < (?)));';
-     db.query(sql, [i,startdate , enddate], function (err, data, fields) {
+    db.query(sql, [i,startdate , enddate], function (err, data, fields) {
         if(err) throw err
-        data .forEach(function(v){ max[i] = parseInt(v.counter); })
-        console.log(max[i]);
+        data .forEach(function(v){ max.push(parseInt(v.counter)); })
     });
-    if( 0 !== --pending ) 
-        break;
+
         
-}   */
-    getmaximum( function(max){
-        console.log(max);
-    });
+}
+function function2() {
+    // all the stuff you want to happen after that pause
+    console.log('finally');
+    console.log(max[0])
     res.render('booking.ejs',{maximum : max});
+    console.log('Blah blah blah blah extra-blah');
+}
+
+
+        setTimeout( function2, 5000);
+    
+      
+    
+   
 
 });
 
